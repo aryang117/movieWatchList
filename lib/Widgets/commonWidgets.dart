1ea632d5e2fbcb215064bf666e90434c91189878
Trolled00Gamer <aryu100@gmail.com>
2021-08-18 18:42:17 +0530

    contains common widgets used throughout the widgets

---
+++ lib/Widgets/commonWidgets.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// This File contains widgets that are being used throughout the app
+// and are very similar to each other, therefore they are being standardized
+
+// TextFormField being used throughout the app
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField(
+      {Key? key, required this.textEditingController, required this.label})
+      : super(key: key);
+
+  final TextEditingController textEditingController;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+    );
+  }
+}
