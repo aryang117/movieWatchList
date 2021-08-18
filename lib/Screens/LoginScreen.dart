bfecf4c2a8236c87b862b353832604b507ba3a20
Trolled00Gamer <aryu100@gmail.com>
2021-08-18 16:21:08 +0530

    added barebones anon sign-in via firebase auth to login screen

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yellowclassactual/Screens/HomeScreen.dart';
 import 'package:yellowclassactual/main.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,47 +13,89 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final Future<FirebaseApp> _initFB = Firebase.initializeApp();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<bool> _signInAnonymously() async {
+    bool _success = true;
+    try {
+      final User? user = (await _firebaseAuth.signInAnonymously()).user;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Signed in Anonymously as user ${user?.uid}'),
+        ),
+      );
+      _success = true;
+    } catch (e) {
+      _success = false;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to sign in Anonymously'),
+        ),
+      );
+    }
+
+    return _success;
+  }
+
   @override
   Widget build(BuildContext context) {
     //width of elements
     final double widthElements = MediaQuery.of(context).size.width - 50;
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 60,
-              width: widthElements,
-              child: MaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                color: Colors.black,
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => MyHomePage()));
-                },
-                child: Text('Sign In!',
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white)),
+    return StreamBuilder<User?>(
+        stream: _firebaseAuth.authStateChanges(),
+        builder: (context, snapshot) {
+          print("snapshotdata" + snapshot.data.toString());
+          if (snapshot.hasData) {
+            print(snapshot.data);
+            return MyHomePage(isUserSignedIn: true);
+          } else
+            return Scaffold(
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 60,
+                      width: widthElements,
+                      child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        color: Colors.black,
+                        onPressed: () async {
+                          bool _success = await _signInAnonymously();
+
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  MyHomePage(isUserSignedIn: _success)));
+                        },
+                        child: Text('Sign In!',
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white)),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                MyHomePage(isUserSignedIn: false)));
+                      },
+                      child: Text(
+                        'Skip for now',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.grey[700],
+                            fontSize: 16),
+                      )),
+                ],
               ),
-            ),
-          ),
-          TextButton(
-              onPressed: () {},
-              child: Text(
-                'Skip for now',
-                style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.grey[700],
-                    fontSize: 16),
-              )),
-        ],
-      ),
-    );
+            );
+        });
   }
 }
