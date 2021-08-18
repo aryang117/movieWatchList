fa62ca862c0d2aab00885e1936e179c292143fa3
Trolled00Gamer <aryu100@gmail.com>
2021-08-18 18:27:20 +0530

    now the poster updates when the movie name field is submmitted or the cta is pressed

--- lib/Screens/updateDBForm.dart
+++ lib/Screens/updateDBForm.dart
@@ -5,7 +5,12 @@ import 'package:yellowclassactual/API/api.dart';
 
 import '/Models/movieDB.dart';
 
+//store the url of the Poster
 String _posterURL = "";
+
+// stores the status of the updation of posterURL, happens when the user may directly
+// click on the update values and not save the field (i.e click on the cta and not submit the value)
+bool _isPosterUpdated = false;
 
 //TODO : Add the api functionality to this one too! Currently it just returns a static link to Halo Infinite's cover photo, not the movie poster
 
@@ -23,30 +28,44 @@ class _UpdateDBFormState extends State<UpdateDBForm> {
   final TextEditingController _movieNameController = TextEditingController();
   final TextEditingController _dirNameController = TextEditingController();
 
+  // gets the values for the current index and assigns them to the fields
+  Future<void>? _getcurrentValues() async {
+    final box = Hive.box('movieDB');
+
+    final curValues = await box.getAt(widget.index) as MovieDB;
+    _movieNameController.text = curValues.movieName;
+    _dirNameController.text = curValues.directorName;
+    _posterURL = curValues.posterLink;
+  }
+
   @override
   void initState() {
     super.initState();
-    final box = Hive.box('movieDB');
-
-    final curValues = box.getAt(widget.index) as MovieDB;
-    _movieNameController.text = curValues.movieName;
-    _dirNameController.text = curValues.directorName;
+    _getcurrentValues();
   }
 
-  //returns moviePosterLink
-  Future<String> _returnMoviePosterLink() async {
-    return await getMoviePoster(_movieNameController.text);
+  // when the user is done with editing the field, it gets the movie Poster Link
+  Future<void> _getMoviePosterLink() async {
+    _posterURL = await getMoviePoster(_movieNameController.text);
+    _isPosterUpdated = true;
+
+    // in case if the movie poster link is given as N/A (movie poster not present/found by api) : edge case
+    if (_posterURL == "N/A")
+      _posterURL =
+          "https://cdn.mos.cms.futurecdn.net/j6reMf3QEuGWFE7FkVmoyT-1200-80.jpg";
+
+    //updating the UI as we have a new poster
+    setState(() {
+      print("updated poster!");
+    });
   }
 
   //updates values in the DB
   Future<void> _updateValues() async {
-    _posterURL = await _returnMoviePosterLink();
+    if (!_isPosterUpdated) await _getMoviePosterLink();
+
     final _updatedMovieData =
         MovieDB(_movieNameController.text, _dirNameController.text, _posterURL);
-
-    if (_posterURL == "N/A")
-      _posterURL =
-          "https://cdn.mos.cms.futurecdn.net/j6reMf3QEuGWFE7FkVmoyT-1200-80.jpg";
 
     setState(() {
       Hive.box('movieDB').putAt(widget.index, _updatedMovieData);
@@ -73,18 +92,18 @@ class _UpdateDBFormState extends State<UpdateDBForm> {
                   height: 250,
                   width: 175,
                   child: Card(
-                    // child: FadeInImage(
-                    //     image: NetworkImage(posterURL),
-                    //     fit: BoxFit.fill,
-                    //     placeholder: NetworkImage(
-                    //         'https://pbs.twimg.com/media/E7VuaNRXEAEloYi.jpg')),
-                    color: Colors.white,
+                    child: FadeInImage(
+                        image: NetworkImage(_posterURL),
+                        fit: BoxFit.fill,
+                        placeholder: NetworkImage(
+                            'https://pbs.twimg.com/media/E7VuaNRXEAEloYi.jpg')),
                     elevation: 5,
                   ),
                 ),
               ),
               _vertPaddingbetweenElements(),
               TextFormField(
+                onEditingComplete: _getMoviePosterLink,
                 controller: _movieNameController,
                 decoration: InputDecoration(
                     labelText: 'Movie Name',
